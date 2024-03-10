ea443ae92d260de404c30a0ad9aa6c15c1ca67d8
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-10 19:57:04 +0800

    Create `main.swift`
    This is command-based shell

---
+++ src/main.swift
@@ -0,0 +1,70 @@
+import Foundation
+
+// Define a struct to represent a command
+struct Command {
+    let name: String
+    let handler: ([String]) -> Void // Change the handler to accept arguments
+}
+
+// Define a struct to represent the shell
+struct Shell {
+    var commands: [String: Command]
+    
+    // Function to register commands
+    mutating func register(command: Command) {
+        commands[command.name] = command
+    }
+    
+    // Function to execute a command
+    func execute(commandName: String, arguments: [String]) {
+        if let command = commands[commandName] {
+            command.handler(arguments)
+        } else {
+            print("Command not found: \(commandName)")
+        }
+    }
+    
+    // Function to start the shell
+    func start() {
+        print("Welcome to Swift Shell!")
+        while true {
+            print("Enter a command:")
+            if let input = readLine() {
+                let components = input.split(separator: " ")
+                if let commandName = components.first {
+                    let arguments = components.dropFirst().map(String.init)
+                    execute(commandName: String(commandName), arguments: arguments)
+                }
+            }
+        }
+    }
+}
+
+// Example commands
+func helpCommand(arguments: [String]) {
+    print("Available commands:")
+    print("help - Display available commands")
+    print("hello - Print Hello World!")
+    print("cat <file> [OPTION]... - Display content of a file")
+    print("ls [OPTION]... [FILE]... - List directory contents")
+}
+
+func helloCommand(arguments: [String]) {
+    print("Hello, World!")
+}
+
+// Create a shell instance
+var shell = Shell(commands: [:])
+
+// Register commands
+shell.register(command: Command(name: "help", handler: helpCommand))
+shell.register(command: Command(name: "hello", handler: helloCommand))
+
+// Register the cat command
+shell.register(command: Command(name: "cat", handler: catCommand))
+
+// Register the ls command
+shell.register(command: Command(name: "ls", handler: lsCommand))
+
+// Start the shell
+shell.start()
