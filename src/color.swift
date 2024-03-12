752d0db89a8e1a4547837b1c22f42c04a95e275a
Cyril John Magayaga <93117076+Magayaga@users.noreply.github.com>
2024-03-12 15:47:54 +0800

    Update `color.swift`

--- src/color.swift
+++ src/color.swift
@@ -4,6 +4,9 @@ import Foundation
 struct ANSIColors {
     static let green = "\u{001B}[32m"
     static let blue = "\u{001B}[34m"
+    static let yellow = "\u{001B}[33m"
+    static let magenta = "\u{001B}[35m"
+    static let cyan = "\u{001B}[36m"
     static let reset = "\u{001B}[0m"
 }
 
